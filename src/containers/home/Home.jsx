d0723eb14880c137f9d7806db75621396c21a985
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-11-15 12:01:27 -0300

    Subindo  projeto inicial

---
+++ src/containers/home/Home.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';
+import { Button, TextField, Box } from '@mui/material';
+import CenteredContainer from "../../components/CenteredContainer.jsx"
+import "../../style/home/Home.css"
+
+const Home = () => {
+  const [crm, setCrm] = useState('');
+  const [isValidCrm, setIsValidCrm] = useState(true);
+  const [crmData, setCrmData] = useState(null);
+  const [crmDataNotFound, setCrmDataNotFound] = useState(false);
+
+
+  const getCrmData = (crm) => {
+    //implementar serviço de dados aqui
+    //Dado estático
+    console.log(crm)
+    if(crm.toUpperCase() == "00000-ES")
+    {
+      return {
+        nome: "NomeTeste C. Sobrenome",
+        especializacao: "Cirurgião",
+        estadoEmissao: "Espírito Santo",
+        emissao: "11/10/2021",
+        status: "Inativo", // ou "Ativo"
+      }
+    }else if(crm.toUpperCase() == "11111-ES"){
+      console.log("0000000")
+      return {
+        nome: "NomeTeste C. Sobrenome",
+        especializacao: "Obstetra",
+        estadoEmissao: "Espírito Santo",
+        emissao: "12/09/2000",
+        status: "Ativo", // ou "Ativo"
+      }
+    }
+    else{
+      return null;
+    }
+  };
+
+  const handleCrmChange = (event) => {
+    setCrm(event.target.value);
+  };
+
+  const validateCrm = () => {
+    const regex = /^\d{5,}(-|\/)(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)$/;
+    const isValidCrm = regex.test(crm.toUpperCase())
+
+    setIsValidCrm(isValidCrm);
+    setCrmDataNotFound(false);
+    setCrmData(null);
+
+    if(isValidCrm)
+    {
+      let crmData = getCrmData(crm);
+      if(crmData)
+        setCrmData(crmData);
+      else
+        setCrmDataNotFound(true);
+    }
+  };
+
+
+  const redirectToLogin = () => {
+   
+    
+  };
+
+  return (
+    <CenteredContainer>
+        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" width={500} className="container-home">
+            <Box>
+                <img width={240} src="src/assets/SecureHealthTransparente.png" alt="Imagem Centralizada" />
+            </Box>
+            <TextField 
+                label="Digite o CRM" 
+                variant="filled" 
+                value={crm} 
+                onChange={handleCrmChange} 
+                error={!isValidCrm}
+                helperText={!isValidCrm ? "CRM inválido" : ""}
+                fullWidth 
+                className="input-crm"
+            />
+            {crmDataNotFound && (
+              <Box className={`crm-data`}>
+                <center>Informações do profissional não encontrado.</center>
+              </Box>
+            )}
+
+           {crmData && (
+              <Box className={`crm-data ${crmData.status.toLowerCase()}`}>
+                <div><strong>Nome:</strong> {crmData.nome}</div>
+                <div><strong>Especialização:</strong> {crmData.especializacao}</div>
+                <div><strong>Estado de emissão:</strong> {crmData.estadoEmissao}</div>
+                <div><strong>Emissão:</strong> {crmData.emissao}</div>
+                <div><strong>Status:</strong> <span className={crmData.status.toLowerCase()=="inativo"?"tagRed":"tagGreen"}>{crmData.status}</span></div>
+              </Box>
+            )}
+
+            <Box className='group-button-home'>
+                <Button variant="contained" color="primary" onClick={validateCrm} className='button-home'>
+                    Verificar CRM
+                </Button>
+                <Button variant="contained" color="success" onClick={redirectToLogin} className='button-home'>
+                    Ir para Login
+                </Button>
+            </Box>
+        </Box>
+        
+    </CenteredContainer>
+  );
+};
+
+export default Home;
